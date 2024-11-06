ef06293cb4b01cb8ab8573851f70bc69a1d1fec9
Karuna Nidhan Singh <130839938+KARUNANS2004@users.noreply.github.com>
2024-11-06 16:20:31 +0530

    Update TodoInput.jsx

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -4,10 +4,16 @@ export function TodoInput(props){
     const {handleAddTodo}=props
     const [inputValue, setInputValue]=useState('');
     // the setter functions are designed to update the value automatically
+    const handleKeyDown=(event)=>{
+        if(event.key==="Enter"){
+            handleAddTodo(inputValue)
+            setInputValue('')
+        }
+    }
 
     return (
         <div className="input-container">
-            <input value={inputValue} onChange={(event)=>{setInputValue(event.target.value)}} placeholder="Add task" />
+            <input value={inputValue} onChange={(event)=>{setInputValue(event.target.value)}} onKeyDown={handleKeyDown} placeholder="Add task" />
             <button onClick={()=>{
                 if(!inputValue) {return}
                 handleAddTodo(inputValue)
